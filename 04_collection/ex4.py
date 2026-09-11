11342f350339964194a93b9d36d028f7dc07ff7e
JoPopCorN_incode <corninamicrowave.gmail.com>
2026-09-11 14:17:39 +0900

    09/11

--- 04_collection/ex4.py
+++ 04_collection/ex4.py
@@ -132,5 +132,14 @@ stocks = (
 
 # 총 재고 금액 출력
 total=sum(price*qty for _,price,qty in stocks)
-print(f"총액: {total}")
-#print(f"총액: {sum(tuple(i[1]*i[2] for i in stocks)):,d}원")                   # ✅ 총액: 21,000원
+print(f"총액: {total:,}원")
+# print(f"총액: {sum(tuple(i[1]*i[2] for i in stocks)):,d}원")                   # ✅ 총액: 21,000원
+
+stocks = (
+    ("사과", "바나나", "체리"),
+    (1000, 2000, 5000),
+    (5, 3, 2),
+)
+
+total=sum(price*qty for _,price,qty in zip(*stocks))
+print(f"총액: {total:,}원")
